fda8fc2272f8686134d5d36326319f3f30d52471
nelson-alex <113587717+nelson-alex@users.noreply.github.com>
2022-10-04 15:42:36 +0300

    Add files via upload

---
+++ adding numbers btn 1 and 200.cpp
@@ -0,0 +1,12 @@
+  //program to add numbers btn 1 and 200
+#include <iostream>
+using namespace std;
+int main() {
+     int i,sum;
+     for(i=0;i<=200;i+=1){
+         sum=sum+i;
+     }
+         cout  <<"  sum is "<< sum    ;
+
+    return 0;
+}
