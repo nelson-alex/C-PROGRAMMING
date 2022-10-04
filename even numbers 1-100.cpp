e22ff0dfb9705704dc076e9e31962629d5461257
nelson-alex <113587717+nelson-alex@users.noreply.github.com>
2022-10-04 15:41:50 +0300

    Add files via upload

---
+++ even numbers 1-100.cpp
@@ -0,0 +1,11 @@
+ //program to print even numbers between 0 and 100
+#include <iostream>
+using namespace std;
+int main() {
+     int i,sum;
+     for(i=0;i<=100;i+=2){
+         cout  <<i<<"    " ;
+}
+
+    return 0;
+}
