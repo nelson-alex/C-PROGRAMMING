e50f68f05febd8b97a25ad6fb0a16eef1534c1c5
nelson-alex <113587717+nelson-alex@users.noreply.github.com>
2022-10-07 17:15:47 +0300

    Add files via upload

---
+++ odd numbers.cpp
@@ -0,0 +1,13 @@
+//program to print odd numbers btn 1 & 200
+ #include <iostream>
+using namespace std;
+int main() {
+ int i = 1;
+  
+ while (i <= 200) {
+ cout << i  <<  "  ";
+ i=i+2;
+ }
+
+ return 0;
+}
