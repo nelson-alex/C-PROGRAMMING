bdd60149c10802c84ef20641f56771e003d023e2
nelson-alex <113587717+nelson-alex@users.noreply.github.com>
2022-10-07 17:19:32 +0300

    Add files via upload

---
+++ even numbers.cpp
@@ -0,0 +1,11 @@
+//program to print even numbers btn 1 & 200
+ #include <iostream>
+using namespace std;
+int main() {
+ int i = 2;
+ while (i <= 200) {
+ cout << i  <<  "  ";
+ i=i+2;
+ }
+return 0;
+}
